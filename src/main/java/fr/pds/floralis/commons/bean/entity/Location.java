4979e33aeeaa77855833ee8a9b30c6d20a0f4ecb
laura <laura.alves.fe@gmail.com>
2019-03-19 16:20:07 +0100

    Create Location.java

---
+++ src/main/java/fr/pds/floralis/commons/bean/entity/Location.java
@@ -0,0 +1,63 @@
+package fr.pds.floralis.commons.bean.entity;
+
+import fr.pds.floralis.commons.bean.entity.type.TypeFloor;
+
+public class Location {
+
+	private int id;
+	private Sensor sensor[];
+	private Room room;
+	private TypeFloor floor;
+	private Building building;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Sensor[] getSensor() {
+		return sensor;
+	}
+
+	public void setSensor(Sensor[] sensor) {
+		this.sensor = sensor;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public TypeFloor getFloor() {
+		return floor;
+	}
+
+	public void setFloor(TypeFloor floor) {
+		this.floor = floor;
+	}
+
+	public Building getBuilding() {
+		return building;
+	}
+
+	public void setBuilding(Building building) {
+		this.building = building;
+	}
+
+	public Location(int id, Sensor[] sensor, Room room, TypeFloor floor,
+			Building building) {
+		super();
+		this.id = id;
+		this.sensor = sensor;
+		this.room = room;
+		this.floor = floor;
+		this.building = building;
+	}
+
+}
