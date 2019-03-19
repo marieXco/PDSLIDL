44f54ac4f150bd0ae7d477fbe0f8627f0c5cd15a
laura <laura.alves.fe@gmail.com>
2019-03-19 18:20:10 +0100

    Update SensorsTableModel.java

--- src/main/java/fr/pds/floralis/gui/tablemodel/SensorsTableModel.java
+++ src/main/java/fr/pds/floralis/gui/tablemodel/SensorsTableModel.java
@@ -5,33 +5,23 @@ import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
-<<<<<<< HEAD
+
 import fr.pds.floralis.commons.bean.entity.Sensor;
-=======
->>>>>>> 3428ca928c2eeb2172880a930c6dfb82f884de59
-import fr.pds.floralis.commons.bean.entity.Sensors;
+
 
 public class SensorsTableModel extends AbstractTableModel {
 
-<<<<<<< HEAD
+
 	private List<Sensor> userData = new ArrayList<Sensor>();
 	private String[] columnNames = { "Marque", "Adresse Mac", "Date Installation", "Etat",
-=======
-	private List<Sensors> userData = new ArrayList<Sensors>();
-	private String[] columnNames = { "Marque", "AdresseMac", "DateInstallation", "Etat", "En alerte", "En panne",
->>>>>>> 3428ca928c2eeb2172880a930c6dfb82f884de59
 			"Caractéristiques" };
 
 	public SensorsTableModel() {
 	}
 
-<<<<<<< HEAD
+
 	public SensorsTableModel(List<Sensor> sensorsList) {
 		this.userData = sensorsList;
-=======
-	public SensorsTableModel(List<Sensors> userData) {
-		this.userData = userData;
->>>>>>> 3428ca928c2eeb2172880a930c6dfb82f884de59
 	}
 
 	@Override
@@ -52,7 +42,6 @@ public class SensorsTableModel extends AbstractTableModel {
 	@Override
 	public Object getValueAt(int row, int column) {
 		Object userAttribute = null;
-<<<<<<< HEAD
 		Sensor userObject = userData.get(row);
 		switch (column) {
 		case 0:
@@ -68,29 +57,6 @@ public class SensorsTableModel extends AbstractTableModel {
 			userAttribute = userObject.getState();
 			break;
 		case 4:
-=======
-		Sensors userObject = userData.get(row);
-		switch (column) {
-		case 1:
-			userAttribute = userObject.getBrand();
-			break;
-		case 2:
-			userAttribute = userObject.getMacAddress();
-			break;
-		case 3:
-			userAttribute = userObject.getDateInstallation();
-			break;
-		case 4:
-			userAttribute = userObject.getState();
-			break;
-		case 5:
-			userAttribute = userObject.getAlert();
-			break;
-		case 6:
-			userAttribute = userObject.getBreakdown();
-			break;
-		case 7:
->>>>>>> 3428ca928c2eeb2172880a930c6dfb82f884de59
 			userAttribute = userObject.getCaracteristics();
 			break;
 		default:
@@ -99,11 +65,8 @@ public class SensorsTableModel extends AbstractTableModel {
 		return userAttribute;
 	}
 
-<<<<<<< HEAD
+
 	public void addUser(Sensor user) {
-=======
-	public void addUser(Sensors user) {
->>>>>>> 3428ca928c2eeb2172880a930c6dfb82f884de59
 		userData.add(user);
 		fireTableDataChanged();
 	}
