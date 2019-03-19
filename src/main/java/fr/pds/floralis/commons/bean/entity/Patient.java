9285c9c761ba720487f4811007b4a671cca766cb
laura <laura.alves.fe@gmail.com>
2019-03-19 16:20:11 +0100

    Create Patient.java

---
+++ src/main/java/fr/pds/floralis/commons/bean/entity/Patient.java
@@ -0,0 +1,12 @@
+package fr.pds.floralis.commons.bean.entity;
+
+public class Patient extends Person{
+
+	public Patient(int id, String lastName, String firstName, Code code) {
+		super(id, lastName, firstName, code);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
