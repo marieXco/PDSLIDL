a07dfc424ab1543d0b7cfa3024248eec18f38622
laura <laura.alves.fe@gmail.com>
2019-03-19 16:20:16 +0100

    Create Personnel.java

---
+++ src/main/java/fr/pds/floralis/commons/bean/entity/Personnel.java
@@ -0,0 +1,14 @@
+package fr.pds.floralis.commons.bean.entity;
+
+
+public class Personnel extends Staff{
+
+	public Personnel(int id, String lastName, String firstName, Code code) {
+		super(id, lastName, firstName, code);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	
+}
