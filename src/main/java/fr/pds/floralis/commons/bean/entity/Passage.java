8703bdfc651cca7e6dafd753153fef9ed6a2253e
laura <laura.alves.fe@gmail.com>
2019-03-19 16:20:09 +0100

    Create Passage.java

---
+++ src/main/java/fr/pds/floralis/commons/bean/entity/Passage.java
@@ -0,0 +1,46 @@
+package fr.pds.floralis.commons.bean.entity;
+
+import java.sql.Date;
+
+import fr.pds.floralis.commons.bean.entity.type.TypePassage;
+
+public class Passage {
+
+	private int id;
+	private Building building;
+	private TypePassage type;
+	private Date date;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Building getBuilding() {
+		return building;
+	}
+
+	public void setBuilding(Building building) {
+		this.building = building;
+	}
+
+	public TypePassage getType() {
+		return type;
+	}
+
+	public void setType(TypePassage type) {
+		this.type = type;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
