152381b3b37a68badbbf82002b35a00024ec6ad4
CeliaMessaoudi <celia.messaoudi.cm@gmail.com>
2019-04-21 20:47:19 +0200

    Update AlertDao.java and Alert.java

--- src/main/java/fr/pds/floralis/server/dao/AlertDao.java
+++ src/main/java/fr/pds/floralis/server/dao/AlertDao.java
@@ -2,111 +2,173 @@ package fr.pds.floralis.server.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONObject;
 import org.postgresql.util.PGobject;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import fr.pds.floralis.commons.bean.entity.Alert;
+import fr.pds.floralis.commons.bean.entity.type.TypeLevel;
 
-public class AlertDao extends DAO<Alert> {
-	Connection connect = null; 
-
-	public AlertDao(Connection connect) throws ClassNotFoundException, SQLException {
-		this.connect = connect; 
-	}
+public class AlertDao implements DAO<Alert> {
+	
+	//create a connection 
+		Connection connect = null;
+		
+		//constructor
+		public AlertDao(Connection connect) throws ClassNotFoundException, SQLException {
+			this.connect = connect;  
+		}
 
 	@Override
-	public JSONObject create(JSONObject jsonObject) {
+	public boolean create(Alert alertToCreate) {
+		JSONObject jsonObject = alertToCreate.toJSON();
 		int success = 0;
-
+  
 		PGobject object = new PGobject();
 		try {
 			object.setValue(jsonObject.toString());
 		} catch (SQLException e1) {
 			e1.printStackTrace();
-		}
+		} 
 		object.setType("json");
-
-		try {
-			connect.setAutoCommit(false);
-
+ 
+		try {  
+			connect.setAutoCommit(false);  
 			String sql = "INSERT INTO history_alerts (data) VALUES (?);";
+ 
+			PreparedStatement statement = connect.prepareStatement(sql);  
+			statement.setObject(1, object); 
+			success = statement.executeUpdate(); 
+			connect.commit(); 
+			statement.close(); 
+		} catch (Exception e) { 
+			System.err.println(e.getClass().getName() + ": " + e.getMessage()); 
+			System.exit(0); 
+		} 
+ 
+		if(success > 0) { 
+			return true; 
+		} else { 
+			return false; 
+		} 
+	} 
 
-			PreparedStatement statement = connect.prepareStatement(sql);
+	@Override
+	public boolean delete(int id) {
+		int success = 0;  
+		 
+		try { 
+			connect.setAutoCommit(false); 
+ 
+			String sql = "DELETE FROM hystory_alerts where (data -> 'id')::json::text = '"+ id + "'::json::text;";
+ 
+			PreparedStatement statement = connect.prepareStatement(sql); 
+			success = statement.executeUpdate(); 
+			connect.commit(); 
+			statement.close(); 
+ 
+		} catch (Exception e) { 
+			System.err.println(e.getClass().getName() + ": " + e.getMessage()); 
+			System.exit(0); 
+		} 
+		 
+		if(success > 0) { 
+			return true; 
+		} else { 
+			return false; 
+		} 
+	}
 
-			statement.setObject(1, object);
-			success = statement.executeUpdate();
+	@Override
+	public boolean update(int id, Alert alertToUpdate) {
+		int success = 0; 
+
+		JSONObject jsonObject = alertToUpdate.toJSON();
+ 
+		try {  
+			connect.setAutoCommit(false); 
+			String sql = "UPDATE history_alerts SET data = '" + jsonObject+ "' WHERE (data -> 'id')::json::text = '" + id + "'::json::text;"; 
+			
+			PreparedStatement statement = connect.prepareStatement(sql); 
+			success = statement.executeUpdate();  
 			connect.commit();
 			statement.close();
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
-		}
+ 
+		} catch (Exception e) { 
+			System.err.println(e.getClass().getName() + ": " + e.getMessage()); 
+			System.exit(0); 
+		} 
+ 
+		if(success > 0) {
+			return true; 
+		} else { 
+			return false; 
+		}  
+	}
+ 
+	@Override
+	public Object find(int id) {
+		ObjectMapper mapper = new ObjectMapper(); 
+		List<Integer> emptyList = new ArrayList<Integer>(); 
+		emptyList.add(0); 
+		emptyList.add(0); 
+		Alert alert = new Alert(0, TypeLevel.LEVEL1, 0, null, null);
 
-		JSONObject alertCreated = new JSONObject();
-		
-		if (success > 0) {
-			alertCreated.put("successCreate", "true");
-		} else {
-			alertCreated.put("successCreate", "false");
-		}
-		
-		
-		return alertCreated;
+		try {  
+			connect.setAutoCommit(false); 
+			Statement stmt = connect.createStatement(); 
+			ResultSet rs = stmt.executeQuery("SELECT data FROM history_alerts where (data -> 'id')::json::text = '"+ id + "'::json::text;");
+			
+			while (rs.next()) {
+				alert = mapper.readValue(rs.getObject(1).toString(), Alert.class);
+			} 
+
+			rs.close(); 
+			stmt.close(); 
+ 
+		} catch (Exception e) { 
+			System.err.println(e.getClass().getName() + ": " + e.getMessage()); 
+			System.exit(0); 
+		} 
+  
+		return alert;
+
 	}
 
 	@Override
-	public JSONObject delete(JSONObject jsonObject) {
-		int success = 0;
-		int alertId = jsonObject.getInt("id"); 
+	public List<Alert> findAll() {
+		ObjectMapper mapper = new ObjectMapper(); 
+		List<Alert> alertList = new ArrayList<Alert>(); 
+		Alert alert = new Alert(0, TypeLevel.LEVEL1, 0, null, null);
+ 
+		try { 
+			connect.setAutoCommit(false); 
+			Statement stmt = connect.createStatement();
 
-		
-		try {
-			connect.setAutoCommit(false);
-
-			String sql = "DELETE FROM history_alerts where (data -> 'id')::json::text = '"
-					+ alertId + "'::json::text;";
-
-			PreparedStatement statement = connect.prepareStatement(sql);
-
-			success = statement.executeUpdate();
-			connect.commit();
-			statement.close();
-
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
-		}
-		
-		JSONObject alertDeleted = new JSONObject();
-		
-		if (success > 0) {
-			alertDeleted.put("successDelete", "true");
-		} else {
-			alertDeleted.put("successDelete", "false");
-		}
-	
-		return alertDeleted;
+			ResultSet rs = stmt.executeQuery("SELECT data FROM history_alerts;");
+ 
+			while (rs.next()) { 
+				alert = mapper.readValue(rs.getObject(1).toString(), Alert.class);
+				alertList.add(alert);
+			} 
+ 
+			rs.close(); 
+			stmt.close(); 
+ 
+		} catch (Exception e) { 
+			System.err.println(e.getClass().getName() + ": " + e.getMessage()); 
+			System.exit(0); 
+		} 
+ 
+		return alertList;
 	}
 
-
-	@Override
-	public JSONObject update(JSONObject jsonObject) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public JSONObject find(JSONObject jsonObject) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public JSONObject findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
