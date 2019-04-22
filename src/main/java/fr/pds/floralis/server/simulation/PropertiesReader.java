873f3cf8cbff1b91e5630cb377a3dd0a9b860c65
laura <laura.alves.fe@gmail.com>
2019-04-22 02:03:31 +0200

    Adding files for the Use Case

---
+++ src/main/java/fr/pds/floralis/server/simulation/PropertiesReader.java
@@ -0,0 +1,114 @@
+package fr.pds.floralis.server.simulation;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+/**
+ * @author Crunchify.com
+ * 
+ */
+
+public class PropertiesReader {
+	String result = "";
+	InputStream inputStream;
+	List<Entry<String, String>> entryList = new ArrayList<Map.Entry<String, String>>();
+
+	public List<Entry<String, String>> getPropValues() throws IOException {
+
+		try {
+			Properties prop = new Properties();
+			String propFileName = "fire.properties";
+
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+
+
+			/**
+			 * We get the id property and the type property
+			 */
+			
+			String id = prop.getProperty("id");
+			String type = prop.getProperty("type");
+			
+			prop.remove("id");
+			prop.remove("type");
+			
+			Map<String, String> map = new HashMap<String, String>();
+			Map<String, String> list = new LinkedHashMap<String, String>();
+
+			/**
+			 * We put all our key-values pairs in a Map
+			 */
+			for (String key : prop.stringPropertyNames()) {	
+				map.put(key, prop.getProperty(key));
+			}
+
+			String value = "";
+			String duration = "";
+			int index = 0;
+			int numberOfValues = map.size()/2;
+			
+			list.put("id", id);
+			list.put("type", type);
+			
+			
+			
+			while (!(map.isEmpty())) {
+				
+				while(numberOfValues > index) {
+					
+					if(map.containsKey("value.iter." + numberOfValues + ".value")) {
+						value = map.get("value.iter." + numberOfValues + ".value");
+						map.remove("value.iter." + numberOfValues + ".value");		
+					}
+
+					if (map.containsKey("value.iter." + numberOfValues + ".duration")){ 
+						duration = map.get("value.iter." + numberOfValues + ".duration");
+						map.remove("value.iter." + numberOfValues + ".duration");							
+					}
+					
+					list.put(duration, value);
+					numberOfValues--;
+					
+				}
+
+			}
+			
+			/**
+			 * We put our key-value in a list, so we can get the last key-value pair
+			 */
+			 entryList = new ArrayList<Map.Entry<String, String>>(list.entrySet());
+
+			System.out.println("Key-Values pairs : " + entryList.toString() + "\nSensorId : " + id + " \nType : " + type.toUpperCase() + 
+					"\nLastKey (value.iter.1.duration) : " + entryList.get(entryList.size() - 1).getKey() + 
+					"\nLastValue (value.iter.1.value) : " + entryList.get(entryList.size() - 1).getValue());
+
+
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+		
+		return entryList;
+	}
+}
--- src/main/java/fr/pds/floralis/server/simulation/Simulation.java
+++ src/main/java/fr/pds/floralis/server/simulation/Simulation.java
@@ -2,11 +2,13 @@ package fr.pds.floralis.server.simulation;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Logger;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,70 +23,179 @@ import fr.pds.floralis.gui.connexion.ConnectionClient;
 
 public class Simulation {
 
-
 	static ObjectMapper objectMapper = new ObjectMapper();
 
-	public static void simulationTest() throws JsonParseException, JsonMappingException, JSONException, IOException {
-		Request request = new Request();
-		request.setType("FINDALL");
-		request.setEntity("SENSOR");
-		request.setFields(new JSONObject());
-		
-		ConnectionClient cc = new ConnectionClient("127.0.0.1", 2412, request.toString());
-		cc.run();
-		
-		// TODO : instead of all sensors, only on the sensor on the configuration
-		
-		String response = cc.getResponse();
-		JSONObject sensorsFound = new JSONObject();
-		sensorsFound.put("sensorsFound", response);
-		System.out.println(sensorsFound.get("sensorsFound"));
-		
-		Sensor[] sensorsFoundTab =  objectMapper.readValue(sensorsFound.get("sensorsFound").toString(), Sensor[].class);
-		List<Sensor> sensorsList = Arrays.asList(sensorsFoundTab);
-		
+	public static void simulationTest() throws JsonParseException, JsonMappingException, JSONException, IOException, InterruptedException {
+		Logger logger = Logger.getLogger("test");
+		String periodOfDay = "";
+
+		Calendar now = Calendar.getInstance();
+
+		Calendar night = new GregorianCalendar();	
+		night.set(Calendar.HOUR_OF_DAY, 19);
+		night.set(Calendar.MINUTE, 59);
+		night.set(Calendar.SECOND, 59);
+
+		Calendar day = new GregorianCalendar();	
+		day.set(Calendar.HOUR_OF_DAY, 9);
+		day.set(Calendar.MINUTE, 00);
+		day.set(Calendar.SECOND, 00);
+
+		// TODO : get the warning level depending on time and refresh if it changes during simulation
+
+		if(now.before(night) || now.after(day)) {
+			periodOfDay = "DAYTIME";
+			logger.info("We're in daytime : warning levels of daytime");
+		} else {
+			periodOfDay = "NIGHTTIME";
+			logger.info("We're in nighttime : warning levels of nighttime");
+		}
+
+
 		List<Entry<String, String>> entryList = new ArrayList<Map.Entry<String, String>>();
-		
+
 		PropertiesReader properties = new PropertiesReader();
 		entryList = properties.getPropValues();
 		int id = Integer.parseInt(entryList.get(0).getValue());
-		
-		
+
+		entryList.remove(0);
+		entryList.remove(0);
+
+		System.out.println(entryList.toString());
+
+		JSONObject sensorId = new JSONObject();
+		sensorId.put("id", id);
+
+		Request request = new Request();
+		request.setType("FINDBYID");
+		request.setEntity("SENSOR");
+		request.setFields(sensorId);
+
+		ConnectionClient cc = new ConnectionClient("127.0.0.1", 2412, request.toJSON().toString());
+		cc.run();
+
+
+		String response = cc.getResponse();
+		JSONObject sensorFound = new JSONObject();
+		sensorFound.put("sensorFound", response);
+		System.out.println(sensorFound.get("sensorFound"));
+
+		Sensor sensorGotten =  objectMapper.readValue(sensorFound.get("sensorFound").toString(), Sensor.class);
+		List<Sensor> sensorsList = new ArrayList<Sensor>();
+		sensorsList.add(sensorGotten); 
+
 		HashMap<String, Integer> sensorsCache = new HashMap<String, Integer>();
-		
-		for (Sensor sensor : sensorsList) { 
-			// Real sensor, sensor gotten thanks to the find by id from the id of the configuration
-			if(sensor.getState()) {
-				System.out.println("sensor on " + sensor.getId());
-			} else {
-				System.out.println("sensor off " + sensor.getId());
-			}
-			
-			// TODO : not 10 but the value that we got from the config file
-			if(Integer.parseInt(sensor.getMax()) < 10) {
-				sensorsCache.put("ALERTHIGHER", sensor.getId());
-				System.out.println("Type alert : HIGHERMAX " + sensor.getId());
-				// TODO : put it in the cache as an alert type alert : HIGHERMAX
-			}
-			else if(Integer.parseInt(sensor.getMin()) > 10) {
-				sensorsCache.put("ALERTLOWER", sensor.getId());
-				System.out.println("Type alert : BESIDEMIN " + sensor.getId());
-				// TODO : put it in the cache as an alert, type alert : BESIDEMIN
-			}
-			else {
-				sensorsCache.put("NOALERT", sensor.getId());
-				System.out.println("No alert");
-				// TODO : in the cache but no alert
-			}
+
+		System.out.println(Integer.parseInt(sensorGotten.getMin()));
+
+		int duration = Integer.parseInt(entryList.get(entryList.size() - 1).getKey());
+		int value = Integer.parseInt(entryList.get(entryList.size() - 1).getValue());
+		int realTimeValue = 1;
+
+		// TODO : instead of 10 --> get the sensor time before alert thanks to the day/nightTime
+		// TODO : possible alert again when the value changes but still too high ? 
+		// TODO : stop when we find an alert ? 
+		// TODO : update only when the state before is not the new one
+
+		if(sensorGotten.getState()) {		
+			logger.info("Sensor with the id "+ sensorGotten.getId() + " is on");
+
+			while(!entryList.isEmpty()) {
+				duration = Integer.parseInt(entryList.get(entryList.size() - 1).getKey());
+				value = Integer.parseInt(entryList.get(entryList.size() - 1).getValue());
+				realTimeValue = 1;
+
+				if(Integer.parseInt(sensorGotten.getMax()) < value || Integer.parseInt(sensorGotten.getMin()) > value) {
+					while(realTimeValue <= duration) {
+						while(realTimeValue < 10) {
+							if(sensorsCache.containsKey("POSSIBLEALERT")) {
+								sensorsCache.remove("POSSIBLEALERT");
+							}
+
+							sensorsCache.put("POSSIBLEALERT", realTimeValue);
+							logger.warning("Type alert : POSSIBLEALERT for the sensor : " + sensorGotten.getId()  + " for " + 
+									realTimeValue + " seconds with the value " + value);
+							Thread.sleep(1000);
+							realTimeValue++;
+						}
+
+
+
+						sensorsCache.remove("POSSIBLEALERT");
+
+						if(Integer.parseInt(sensorGotten.getMax()) < value) {
+							if(sensorsCache.containsKey("ALERTHIGHER")) {
+								sensorsCache.remove("ALERTHIGHER");
+							}
+
+							sensorsCache.put("ALERTHIGHER", realTimeValue);
+							logger.warning("Type alert : HIGHERMAX for the sensor : " + sensorGotten.getId()  + " for " + 
+									realTimeValue + " seconds with the value " + value);
+							realTimeValue++;
+							Thread.sleep(1000);
+							
+							JSONObject newState = new JSONObject();
+							sensorGotten.setAlert(true);
+							newState.put("id", sensorGotten.getId());
+							newState.put("sensorToUpdate", sensorGotten.toJSON());
+							
+							
+							Request secondRequest = new Request();
+							secondRequest.setType("UPDATE");
+							secondRequest.setEntity("SENSOR");
+							secondRequest.setFields(newState);
+							
+
+							ConnectionClient ccr = new ConnectionClient("127.0.0.1", 2412, secondRequest.toJSON().toString());
+							ccr.run();
+							
+
+						} else {
+
+							if(sensorsCache.containsKey("ALERTLOWER")) {
+								sensorsCache.remove("ALERTLOWER");
+							}
+
+							sensorsCache.put("ALERTLOWER", realTimeValue);
+							logger.info("Type alert : BESIDEMIN for the sensor : " + sensorGotten.getId() + " for " + 
+									realTimeValue + " seconds with the value " + value);
+							realTimeValue++;
+							Thread.sleep(1000);
+						}
+					}
+				}
+
+				else {
+
+					if(sensorsCache.containsKey("NOALERT")) {
+						sensorsCache.remove("NOALERT");
+					}
+
+					while(realTimeValue <= duration) {
+						sensorsCache.put("NOALERT", realTimeValue);
+						logger.info("No alert for the sensor: " + sensorGotten.getId() + " for " + 
+								realTimeValue + " seconds with the value " + value);
+						realTimeValue++;
+						Thread.sleep(1000);
+					}
+
+				}
+
+				entryList.remove(entryList.size() - 1);
+
+			} 
 		}
-		
-		
-		
+		else {
+			logger.info("Sensor with the id "+ sensorGotten.getId() + " is off, no operations done");
+		}
+
+
+
+		System.out.println(sensorsCache.toString());
+
 	}
 
-	public static void main (String[] args) throws JsonParseException, JsonMappingException, JSONException, IOException {
+	public static void main (String[] args) throws JsonParseException, JsonMappingException, JSONException, IOException, InterruptedException {
 		Simulation.simulationTest();
-		
-		
 	}
 }
